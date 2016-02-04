c65d52510635cd000a932bfe57a76ca489f1a478
Cole Tucker <cole.tucker@west-mec.org>
2016-02-03 18:11:24 -0700

    Created the about page fully, fixed the nav bar so it fills the page.

--- About.php
+++ About.php
@@ -1,9 +1,3 @@
-/**
- * Created by PhpStorm.
- * User: session2
- * Date: 2/2/16
- * Time: 5:20 PM
- */
 <?php
 /**
  * Created by PhpStorm.
@@ -27,9 +21,24 @@ if ($conn->connect_error) {
 <!DOCTYPE html>
 <head>
     <title>New York Apartments</title>
+    <link rel="stylesheet" type="text/css" href="styleSheet.css">
 </head>
-<body background="http://www.brandbox.io/wp-content/uploads/2015/07/New-York-1.jpg">
+<body id="About">
+<div class="aboutUl">
+    <ul>
+        <li><a href="home.php">Home</a></li>
+        <li><a href="About.php">About</a></li>
+        <li><a href="Contact.php">Contact</a></li>
+        <li><a href="Products.php">Products</a></li>
+        <li><a href="shoppingCart.php">Shopping Cart</a></li>
+        <li><a href="Checkout.php">Checkout</a></li>
+        <li><a href="F.A.Q..php">F.A.Q.</a></li>
+        <li><a href="Account.php">Account</a></li>
+    </ul>
+</div>
 <h1>About Us</h1>
+<div class="content">
+<div class="aboutContent">
     <p>New York Apartments is a dedicated team of individuals that only finds the most high-end apartments for sale.
        We specialize in many professions, as is needed to bring the best in all aspects relevant to apartments.</p>
 <h2>Mission</h2>
@@ -40,4 +49,6 @@ if ($conn->connect_error) {
        After more than a few tasked hours, they settled on the apartment market.
        Being owners themselves, they thought, why not just use what they know in the market to others advantage?
        They then got to work on the framework of the business today, New York Apartments.</p>
+</div>
+</div>
 </body>
