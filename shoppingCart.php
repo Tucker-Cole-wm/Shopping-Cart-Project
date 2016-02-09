4b01b2b32ae58aa1efa8533427221f1dbe839e95
Cole Tucker <cole.tucker@west-mec.org>
2016-02-08 18:09:55 -0700

    Slight css changes to background of the text.

--- shoppingCart.php
+++ shoppingCart.php
@@ -25,6 +25,26 @@ if ($conn->connect_error) {
 </head>
 
 <body>
+<div class="aboutUl">
+    <ul>
+        <li><a href="home.php">Home</a></li>
+        <li><a href="About.php">About</a></li>
+        <li><a href="Contact.php">Contact</a></li>
+        <li><a href="Products.php">Products</a></li>
+        <li><a href="shoppingCart.php">Shopping Cart</a></li>
+        <li><a href="Checkout.php">Checkout</a></li>
+        <li><a href="F.A.Q..php">F.A.Q.</a></li>
+        <li><a href="Account.php">Account</a></li>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <li class="copyright"> © New York Apartments</li>
+    </ul>
+</div>
 <h1>Shopping Cart</h1>
 
 </body>
