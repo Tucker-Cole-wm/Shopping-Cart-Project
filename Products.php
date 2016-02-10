cf32314167b0b761f6e71c128ee3232fedc08a54
Cole Tucker <cole.tucker@west-mec.org>
2016-02-09 18:10:16 -0700

    Added info into the product table.

--- Products.php
+++ Products.php
@@ -53,23 +53,65 @@ if ($conn->connect_error) {
     <div class="productTable">
     <table border="1px">
         <tr>
-            <td><img src="http://www.idesignarch.com/wp-content/uploads/Remodelled-Rooftop-Apartment-New-York_3.jpg" height="300px" width="300px"></td>
-            <td><img src="http://luxuryrentalsmanhattan.com/sites/default/files/uploads/OneMiMATower.jpg" height="300px" width="300px"></td>
-            <td><img src="https://silversuitesresidences.com/wp-content/uploads/2013/02/2bdrm_a-1000x445.jpg" height="300px" width="300px"></td>
+            <td><img src="http://www.idesignarch.com/wp-content/uploads/Remodelled-Rooftop-Apartment-New-York_3.jpg" height="300px" width="300px">
+                <p>Name:</p>
+                <p>Description:</p>
+                <p>Address:00001 Madison St.</p>
+                <p>Price:$300,000</p></td>
+            <td><img src="http://luxuryrentalsmanhattan.com/sites/default/files/uploads/OneMiMATower.jpg" height="300px" width="300px">
+                <p>Name:</p>
+                <p>Description:</p>
+                <p>Address: 00002 Madison St.</p>
+                <p>Price:$300,000</p></td>
+            <td><img src="https://silversuitesresidences.com/wp-content/uploads/2013/02/2bdrm_a-1000x445.jpg" height="300px" width="300px">
+                <p>Name:</p>
+                <p>Description:</p>
+                <p>Address: 00003 Madison St.</p>
+                <p>Price:$300,000</p></td>
         </tr>
         <tr>
-            <td><img src="http://www.ivettearango.com/wp-content/uploads/2011/10/Davis-Seating-Day-view.jpg" height="300px" width="300px"></td>
-            <td><img src="http://metroloftnyc.com/wp-content/uploads/2009/08/New-York-Apartments-11.jpg" height="300px" width="300px"></td>
-            <td><img src="http://cdn.home-designing.com/wp-content/uploads/2013/04/Clock-Tower-Apartment-neutral-sitting-area-with-views-to-New-York-City-and-statement-soft-furnishings.jpg" height="300px" width="300px"></td>
+            <td><img src="http://www.ivettearango.com/wp-content/uploads/2011/10/Davis-Seating-Day-view.jpg" height="300px" width="300px">
+                <p>Name:</p>
+                <p>Description:</p>
+                <p>Address: 00004 Madison St.</p>
+                <p>Price:$300,000</p>
+            </td>
+            <td><img src="http://metroloftnyc.com/wp-content/uploads/2009/08/New-York-Apartments-11.jpg" height="300px" width="300px">
+                <p>Name:</p>
+                <p>Description:</p>
+                <p>Address: 00005 Madison St.</p>
+                <p>Price:$300,000</p>
+            </td>
+            <td><img src="http://cdn.home-designing.com/wp-content/uploads/2013/04/Clock-Tower-Apartment-neutral-sitting-area-with-views-to-New-York-City-and-statement-soft-furnishings.jpg" height="300px" width="300px">
+                <p>Name:</p>
+                <p>Description:</p>
+                <p>Address: 00006 Madison St.</p>
+                <p>Price:$300,000</p>
+            </td>
         </tr>
         <tr>
-            <td><img src="http://www.blogcdn.com/slideshows/images/slides/304/793/5/S3047935/slug/l/432pa-view-from-dining-room-looking-south-copyright-dbox-for-1.jpg" height="300px" width="300px"></td>
-            <td><img src="http://edc.h-cdn.co/assets/15/08/980x490/landscape_1424208961-unknown-9.jpeg" height="300px" width="300px"></td>
-            <td><img src="http://cdn.myfancyhouse.com/wp-content/uploads/2013/11/Superb-Apartment-in-Soho-New-York-1.jpg" height="300px" width="300px"></td>
+            <td><img src="http://www.blogcdn.com/slideshows/images/slides/304/793/5/S3047935/slug/l/432pa-view-from-dining-room-looking-south-copyright-dbox-for-1.jpg" height="300px" width="300px">
+                <p>Name:</p>
+                <p>Description:</p>
+                <p>Address: 00007 Madison St.</p>
+                <p>Price:$300,000</p>
+            </td>
+            <td><img src="http://edc.h-cdn.co/assets/15/08/980x490/landscape_1424208961-unknown-9.jpeg" height="300px" width="300px">
+                <p>Name:</p>
+                <p>Description:</p>
+                <p>Address: 00008 Madison St.</p>
+                <p>Price:$300,000</p>
+            </td>
+            <td><img src="http://cdn.myfancyhouse.com/wp-content/uploads/2013/11/Superb-Apartment-in-Soho-New-York-1.jpg" height="300px" width="300px">
+                <p>Name:</p>
+                <p>Description:</p>
+                <p>Address: 00009 Madison St.</p>
+                <p>Price:$300,000</p>
+            </td>
         </tr>
     </table>
     </div>
 </div>
 </div>
 </body>
-</html>
+</html>x
